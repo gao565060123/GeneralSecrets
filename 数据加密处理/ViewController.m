3570c40b958b0b40f0d83c2aba77ee622f18530f
gao565060123 <565060123@qq.com>
2016-09-13 09:42:32 +0800

    9-13
    1

---
+++ 数据加密处理/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  数据加密处理
+//
+//  Created by mybook on 16/9/13.
+//  Copyright © 2016年 石塔Mr.GC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
